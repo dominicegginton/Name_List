b4e505d3513b1c5359086bb959512b30025304ae
Dominic <dominic.egginton@gmail.com>
2019-09-27 11:23:25 +0100

    Initial Commit

---
+++ Name_ListTests/Name_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Name_ListTests.swift
+//  Name_ListTests
+//
+//  Created by Dominic Egginton on 27/09/2019.
+//  Copyright © 2019 Dominic Egginton. All rights reserved.
+//
+
+import XCTest
+@testable import Name_List
+
+class Name_ListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
